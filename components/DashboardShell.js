e8599d6dbad94711210707553813a17d1d8b58e8
Tom Champlin <64120886+tomchamplin1@users.noreply.github.com>
2022-07-03 16:15:23 -0500

    updated bg

--- components/DashboardShell.js
+++ components/DashboardShell.js
@@ -9,7 +9,8 @@ const DashboardShell = ({ children }) => {
   const { user, signOut } = useAuth();
 
   return (
-    <Box backgroundColor="gray.100" h="100vh">
+    <Box backgroundColor="gray.100" minH="100vh" backgroundSize="cover" pb={20}>
+      <Box backgroundColor="blue.400" h={2} w="100%" />
       <Flex backgroundColor="white" mb={16} w="full">
         <Flex
           alignItems="center"
