524152c4163c50e927acae1be9e32c2b48168afb
Tom Champlin <64120886+tomchamplin1@users.noreply.github.com>
2022-07-23 16:36:35 -0500

    Update [...site].js

--- pages/site/[...site].js
+++ pages/site/[...site].js
@@ -1,90 +1,73 @@
+import { useRef } from "react";
 import { useRouter } from "next/router";
-import { useRef, useState, useEffect } from "react";
+import { Box, FormControl, Textarea, Button } from "@chakra-ui/react";
+import useSWR, { mutate } from "swr";
 
-import {
-  Button,
-  Input,
-  Box,
-  FormControl,
-  FormLabel,
-  Textarea,
-} from "@chakra-ui/react";
-import { createFeedback } from "@/lib/db";
-import { getAllFeedback, getAllSites, getSite } from "@/lib/db-admin";
 import { useAuth } from "@/lib/auth";
+import { createFeedback } from "@/lib/db";
+import fetcher from "@/utils/fetcher";
 import Feedback from "@/components/Feedback";
 import DashboardShell from "@/components/DashboardShell";
 import SiteHeader from "@/components/SiteHeader";
-import SiteFeedbackTableHeader from "@/components/SiteFeedbackTableHeader";
 import LoginButtons from "@/components/LoginButtons";
 
-export async function getStaticProps(context) {
-  const [siteId, route] = context.params.site;
-  const { feedback } = await getAllFeedback(siteId, route);
-  const { site } = await getSite(siteId);
-
-  return {
-    props: {
-      initialFeedback: feedback,
-      site,
-    },
-    revalidate: 1,
-  };
-}
-
-export async function getStaticPaths() {
-  const { sites } = await getAllSites();
-  const paths = sites.map((site) => ({
-    params: { site: [site.id.toString()] },
-  }));
-
-  return {
-    paths,
-    fallback: true,
-  };
-}
-
-const FeedbackPage = ({ initialFeedback, site }) => {
+const FeedbackPage = () => {
   const { user, loading } = useAuth();
-  const router = useRouter();
   const inputEl = useRef(null);
-  const [allFeedback, setAllFeedback] = useState(initialFeedback);
-  const [siteId, route] = router.query.site;
+  const router = useRouter();
+  const siteAndRoute = router.query?.site;
+  const siteId = siteAndRoute ? siteAndRoute[0] : null;
+  const route = siteAndRoute ? siteAndRoute[1] : null;
+  const feedbackApi = route
+    ? `/api/feedback/${siteId}/${route}`
+    : `/api/feedback/${siteId}`;
 
-  useEffect(() => {
-    setAllFeedback(initialFeedback);
-  }, [initialFeedback]);
+  const { data: siteData } = useSWR(`/api/site/${siteId}`, fetcher);
+  const { data: feedbackData } = useSWR(feedbackApi, fetcher);
+
+  const site = siteData?.site;
+  const allFeedback = feedbackData?.feedback;
 
   const onSubmit = (e) => {
     e.preventDefault();
 
     const newFeedback = {
       siteId,
+      siteAuthorId: site.authorId,
       route: route || "/",
       author: user.name,
       authorId: user.uid,
-      text: inputEl.current.value,
+      text: inputEl.current.value.replace("\n", "\n\n"),
       createdAt: new Date().toISOString(),
       provider: user.provider,
       status: "pending",
     };
 
     inputEl.current.value = "";
-    setAllFeedback([newFeedback, ...allFeedback]);
     createFeedback(newFeedback);
+    mutate(
+      feedbackApi,
+      async (data) => ({
+        feedback: [newFeedback, ...data.feedback],
+      }),
+      false
+    );
   };
 
   const LoginOrLeaveFeedback = () =>
     user ? (
       <Button
         type="submit"
-        isDisabled={router.isFallback}
+        isDisabled={!siteData || !feedbackData}
         backgroundColor="gray.900"
         color="white"
         fontWeight="medium"
         mt={4}
         _hover={{ bg: "gray.700" }}
-        _active={{ bg: "gray.800", transform: "scale(0.95)" }}
+        _active={{
+          bg: "gray.800",
+          transform: "scale(0.95)",
+        }}
       >
         Leave Feedback
       </Button>
@@ -95,33 +78,30 @@ const FeedbackPage = ({ initialFeedback, site }) => {
   return (
     <DashboardShell>
       <SiteHeader
-        isSiteOwner={true}
+        isSiteOwner={site?.authorId === user?.uid}
         site={site}
         siteId={siteId}
         route={route}
       />
       <Box
         display="flex"
+        mx={4}
         flexDirection="column"
         width="full"
-        maxWidth="600px"
-        margin="5"
-        rounded="lg"
-        shadow={5}
+        maxWidth="700px"
       >
         <Box as="form" onSubmit={onSubmit}>
           <FormControl mb={8}>
             <Textarea
               ref={inputEl}
               id="comment"
-              backgroundColor="white"
               placeholder="Leave a comment"
+              isDisabled={!user}
               h="100px"
             />
             {!loading && <LoginOrLeaveFeedback />}
           </FormControl>
         </Box>
-
         {allFeedback &&
           allFeedback.map((feedback, index) => (
             <Feedback
